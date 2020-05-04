95fe2add66243ba6b6cb23d25f8b76c5eb399665
georgios <george.katsandris@yandex.com>
2020-05-04 17:36:17 +0200

    Remove variable with ports entirely and only use heroku PORT environment variable

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const ports = process.env.port || 3000;
 
 app.use(express.static('public'));
 app.set('view engine','ejs')
@@ -9,6 +8,6 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
-app.listen(ports, function () {
+app.listen(PORT, function () {
   console.log('Example app listening on port 3000!')
 })
