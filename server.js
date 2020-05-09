97b0e03820d6fe08556c6afba32aea0cab35219e
georgios <george.katsandris@yandex.com>
2020-05-09 10:38:27 +0200

    Add runes table

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+myport = process.env.PORT || 3000
+
 app.use(express.static('public'));
 app.set('view engine','ejs')
 
@@ -8,6 +10,6 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
-app.listen(process.env.PORT, function () {
-  console.log('Example app listening on port' + process.env.PORT + '!')
+app.listen(myport, function () {
+  console.log('Example app listening on port ' + myport + '!')
 })
